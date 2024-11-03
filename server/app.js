b64ccf04a8ac55cd3452fbe577bda1e5b9f0cf72
ayush844 <ayush.xyz1625@gmail.com>
2024-11-03 17:08:25 +0530

    connecting and disconnecting the socket
    Signed-off-by: ayush844 <ayush.xyz1625@gmail.com>

--- server/app.js
+++ server/app.js
@@ -2,25 +2,54 @@ import express from 'express';
 
 import { Server } from 'socket.io';
 
+import {createServer} from 'http'
+
+import cors from 'cors';
+
 const PORT = 3000;
 
 
 const app = express();
 
-const server = new Server(app);
+const server = createServer(app);
 
-const io = new Server(server);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+});
 
 
 io.on('connection', (socket) => {
     console.log("user connected");
+    console.log(`socket id: ${socket.id}`);
+
+    
+
+    //----------------- disconnecting user ----------------------------
+    socket.on('disconnect', () => {
+        console.log(`user disconnected ${socket.id}`);
+    })
+    //---------------------------------------------------------------
+
 })
+
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true
+}));
+
 
 app.get('/', (req, res) => {
     res.send("hello ji");
 })
 
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`server is listening at port ${PORT}`);
 })
